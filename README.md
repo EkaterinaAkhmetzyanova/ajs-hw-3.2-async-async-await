8e0dbf8bf9124299c5b021dee0fc927a7351dbe8
Ekaterina Akhmetzyanova <a.ekaterina323@gmail.com>
2021-05-15 19:13:29 +0300

    async/await task

---
+++ README.md
@@ -0,0 +1,2 @@
+Домашнее задание к лекции «Promises, async/await». async/await 
+[![Build status](https://ci.appveyor.com/api/projects/status/4lwrb07x31hshx6o?svg=true)](https://ci.appveyor.com/project/EkaterinaAkhmetzyanova/ajs-hw-3-2-async-async-await)
---
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+import GameSavingLoader from '../GameSavingLoader';
+
+test('check loader', async () => {
+  const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}';
+  const received = await GameSavingLoader.load();
+  expect(received).toEqual(data);
+});
